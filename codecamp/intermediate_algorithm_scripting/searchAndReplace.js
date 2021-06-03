6c791243e0e150e9b7f53132dcc86f384c27a708
Springer Jasmin-Joy <js409@hdm-stuttgart.de>
2021-06-03 19:33:57 +0200

    Added some performance tests

--- codecamp/intermediate_algorithm_scripting/searchAndReplace.js
+++ codecamp/intermediate_algorithm_scripting/searchAndReplace.js
@@ -7,6 +7,8 @@
  * Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
  */
 function myReplace(str, before, after) {
+    const start = Date.now()
+    let countBreak = 0
     let result = ""
 
     for (let i = 0; i < str.length; i++) {
@@ -15,6 +17,7 @@ function myReplace(str, before, after) {
             for (let j = 0; j < before.length; j++) {
                 if (str.charAt(j + i) != before.charAt(j)) {
                     isMatch = false
+                    countBreak++;
                     break
                 }
             }
@@ -28,9 +31,14 @@ function myReplace(str, before, after) {
         }
         result += str.charAt(i)
     }
+    const end = Date.now()
+    //console.log(end - start, countBreak)
     return result
 }
 
-console.assert(myReplace("Let us go to the store", "store", "mall") == "Let us go to the mall", "Let us go to the store 'mall'")
+/* fetch("message.txt")
+    .then(response => response.text())
+    .then(data => myReplace(data, "Lorem", "Ficken")) */
+/* console.assert(myReplace("Let us go to the store", "store", "mall") == "Let us go to the mall", "Let us go to the store 'mall'")
 console.assert(myReplace("He is Sleeping on the couch", "Sleeping", "sitting") == "He is Sitting on the couch", "He is Sleeping on the couch")
-console.assert(myReplace("I think we should look up there", "up", "Down") == "I think we should look down there", "I think we should look up there")
+console.assert(myReplace("I think we should look up there", "up", "Down") == "I think we should look down there", "I think we should look up there") */
