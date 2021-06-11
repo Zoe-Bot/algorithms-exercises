ce55e8580a7a1de8e0b7e14c90a9f6c25d0a06fc
Springer Jasmin-Joy <js409@hdm-stuttgart.de>
2021-06-11 19:05:52 +0200

    Added bubblesort

---
+++ searchalg/bubblesort.js
@@ -0,0 +1,39 @@
+function bubblesort(unsorted) {
+    let sorted = []
+    let cache = 0
+
+    while(unsorted.length != 0) {
+        for (let i = 0; i < unsorted.length; i++) {
+            if (unsorted[i] > unsorted[i + 1]) {
+                cache = unsorted[i]
+                unsorted[i] = unsorted[i + 1]
+                unsorted[i + 1] = cache
+            }
+        }
+        sorted.unshift(unsorted[unsorted.length - 1])
+        unsorted.pop()
+    }
+
+    return sorted
+}
+
+
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    // If you don't care about the order of the elements inside
+    // the array, you should sort both arrays here.
+    // Please note that calling sort on an array will modify that array.
+    // you might want to clone your array first.
+
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
+
+//test
+arr = [6, 4, 9, 2]
+console.assert(arraysEqual(bubblesort(arr), [2, 4, 6, 9]), arr)
